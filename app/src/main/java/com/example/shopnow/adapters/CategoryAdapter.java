29d1d4b0381e220b772399b849419ce0ac15de87
Anurag Sharma <sharmaanurag1910@gmail.com>
2022-08-16 21:32:46 +0530

    ShopNow App

--- app/src/main/java/com/example/shopnow/adapters/CategoryAdapter.java
+++ app/src/main/java/com/example/shopnow/adapters/CategoryAdapter.java
@@ -1,6 +1,7 @@
 package com.example.shopnow.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.text.Html;
 import android.view.LayoutInflater;
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.shopnow.R;
+import com.example.shopnow.activities.CategoryActivity;
 import com.example.shopnow.databinding.ItemCategoriesBinding;
 import com.example.shopnow.model.Category;
 
@@ -42,6 +44,17 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Catego
                 .into(holder.binding.image);
 
         holder.binding.image.setBackgroundColor(Color.parseColor(category.getColor()));
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, CategoryActivity.class);
+                intent.putExtra("catId", category.getId());
+                intent.putExtra("categoryName", category.getName());
+                context.startActivity(intent);
+
+            }
+        });
 
     }
 
